9deebb32b53678919ea3bafbb788cdb220ade273
david garcia giron <david.garcia@tecsup.edu.pe>
2019-05-17 20:14:59 -0500

    commit two

---
+++ PokemonGo/Funciones.swift
@@ -0,0 +1,70 @@
+import Foundation
+import UIKit
+import CoreData
+
+func agregarPokemons(){
+    crearPokemon(xnombre: "Pikachu", ximagenNombre: "pikachu")
+    crearPokemon(xnombre: "Meowth", ximagenNombre: "meowth")
+    crearPokemon(xnombre: "Abra", ximagenNombre: "abra")
+    crearPokemon(xnombre: "Bellsprout", ximagenNombre: "bellsprout")
+    crearPokemon(xnombre: "Caterpie", ximagenNombre: "caterpie")
+    crearPokemon(xnombre: "Charmander", ximagenNombre: "charmander")
+    crearPokemon(xnombre: "Dratini", ximagenNombre: "dratini")
+    crearPokemon(xnombre: "Eevee", ximagenNombre: "eevee")
+    crearPokemon(xnombre: "Jigglypuff", ximagenNombre: "jigglypuff")
+    crearPokemon(xnombre: "Mankey", ximagenNombre: "mankey")
+    crearPokemon(xnombre: "Mew", ximagenNombre: "mew")
+    crearPokemon(xnombre: "Pidgey", ximagenNombre: "pidgey")
+    crearPokemon(xnombre: "Pikachu-1", ximagenNombre: "pikachu-1")
+    crearPokemon(xnombre: "Pikachu-2", ximagenNombre: "pikachu-2")
+    crearPokemon(xnombre: "Rattata", ximagenNombre: "rattata")
+    crearPokemon(xnombre: "Snorlax", ximagenNombre: "snorlax")
+    crearPokemon(xnombre: "Psyduck", ximagenNombre: "psyduck")
+    crearPokemon(xnombre: "Squirtle", ximagenNombre: "squirtle")
+    crearPokemon(xnombre: "Venonat", ximagenNombre: "venonat")
+    crearPokemon(xnombre: "Weedle", ximagenNombre: "weedle")
+    crearPokemon(xnombre: "Zubat", ximagenNombre: "zubat")
+    (UIApplication.shared.delegate as! AppDelegate).saveContext()
+}
+
+func crearPokemon(xnombre:String, ximagenNombre:String){
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    let pokemon = Pokemon(context: context)
+    pokemon.nombre = xnombre
+    pokemon.imagenNombre = ximagenNombre
+}
+
+func obtenerPokemons() -> [Pokemon]{
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    do{
+        let pokemons = try context.fetch(Pokemon.fetchRequest()) as! [Pokemon]
+        if pokemons.count == 0 {
+            agregarPokemons()
+            return obtenerPokemons()
+        }
+        return pokemons
+    }catch{}
+    return[]
+}
+
+func obtenerPokemonsAtrapados() -> [Pokemon]{
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    let queryConWhere = Pokemon.fetchRequest() as NSFetchRequest<Pokemon>
+    queryConWhere.predicate = NSPredicate(format: "atrapado == %@", true as CVarArg)
+    do{
+        let pokemons = try context.fetch(queryConWhere) as [Pokemon]
+        return pokemons
+    } catch{}
+    return[]
+}
+
+func obtenerPokemonsNoAtrapados() -> [Pokemon]{
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    let queryConWhere = Pokemon.fetchRequest() as NSFetchRequest<Pokemon>
+    queryConWhere.predicate = NSPredicate(format: "atrapado == %@", false as CVarArg)
+    do{
+        let pokemons = try context.fetch(queryConWhere) as [Pokemon]
+        return pokemons
+    } catch{}
+    return[]
+}
